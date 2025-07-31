1a135d4859d2b8f14e544eabd4cf50a928ecf846
Zeeshanhassan75 <hassan.zeeshan7767@gmail.com>
2025-07-31 22:28:03 +0530

    Initial Commit for Hotel Service

---
+++ src/test/java/com/microservice/hotelservice/Hotel_Service/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.hotelservice.Hotel_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
